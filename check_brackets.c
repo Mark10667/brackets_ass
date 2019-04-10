c611d81581b8e0ae90bda66cd2b60d5e9cd27b88
尹秀晨 <yinxiuchen@MacBook-Pro-6.local>
2019-04-09 22:09:24 -0400

    done

--- check_brackets.c
+++ check_brackets.c
@@ -4,29 +4,55 @@
 int main() {
     int max_line = 105000;
 	char input_buffer[max_line];
-	int input_len = 0;	
-	
-	fgets(input_buffer, max_line, stdin);	
+	int input_len = 0;
+
+	fgets(input_buffer, max_line, stdin);
 	input_buffer [strcspn (input_buffer, "\r\n")] = '\0';  //remove end-of-line characters
 
     input_len = strlen(input_buffer);
     printf("%d characters were read.\n",input_len);
     // printf("The input was: '%s'\n",input_buffer);
 	fflush(stdout);
-    Stack * opening_brackets_stack = createStack(max_line); 
+    Stack * opening_brackets_stack = createStack(max_line);
     for (int position = 0; position < input_len; ++position) {
         char next = input_buffer[position];
-
         if (next == '(' || next == '[' || next == '{') {
             // Process opening bracket, write your code here
+            Bracket top;
+            if(next == '('){
+              top.type = ROUND;
+            }if(next == '['){
+              top.type = SQUARE;
+            }if(next == '{'){
+              top.type = CURLY;
+            }
+            top.position = position;
+            push(opening_brackets_stack, top);
         }
 
         if (next == ')' || next == ']' || next == '}') {
-            // Process closing bracket, write your code here
+          // Process closing bracket, write your code here
+          if(isEmpty(opening_brackets_stack)){
+            printf("My result is: %d\n", position+1);
+            return 1;
+          }
+          Bracket most_recent = pop(opening_brackets_stack);
+          if((most_recent.type==ROUND && next!=')') || (most_recent.type==SQUARE && next!=']') || (most_recent.type==CURLY && next!='}')){
+            printf("My result is: %d\n", position+1);
+            return 1;
+          }
         }
+    }
+    if(isEmpty(opening_brackets_stack)){
+      printf("My result is Success\n");
+      return 0;
+    }else{
+      printf("My result is: %d\n", (opening_brackets_stack->array[0]).position+1);
+      return 1;
     }
 
     // Printing answer, write your code here
-	printf("My result is:\n");
-    return 0;
+  free(opening_brackets_stack->array);
+  free(opening_brackets_stack);
+  return 0;
 }
--- stack.c
+++ stack.c
@@ -1,40 +1,54 @@
-// C-implementation of stack 
-#include "stack.h" 
-  
-// function to create a stack of given capacity. 
+// C-implementation of stack
+#include "stack.h"
+
+// function to create a stack of given capacity.
 Stack* createStack(unsigned capacity){
-	Stack* stack = (Stack*) malloc(sizeof(Stack)); 
-    stack->capacity = capacity; 
-    stack->top = -1; 
+	Stack* stack = (Stack*) malloc(sizeof(Stack));
+    stack->capacity = capacity;
+    stack->top = -1;
     //TODO: Your choice here:
 	//stack->array = (Bracket*) malloc(stack->capacity * sizeof(Bracket));
-	//StackNode* head;	
-    return stack; 
+	//StackNode* head;
+    stack->array = (Bracket*) malloc(stack->capacity * sizeof(Bracket));
+    return stack;
 }
 
 // Stack is full when top index is equal to capacity-1
 int isFull(Stack* stack){
 	//TODO
-	return 0;
+  if(stack->top == stack->capacity-1){
+    printf("The stack is full");
+    return 1;
+  }
+  return 0;
 }
 
 // Stack is empty when top is equal to -1 (or head of the list is NULL)
 int isEmpty(Stack* stack){
 	//TODO
-	return 0;
+  if(stack->top == -1){
+    printf("The stack is empty");
+    return 1;
+  }
+  return 0;
 }
 
-// Function to add an item to stack.  
+// Function to add an item to stack.
 void push(Stack* stack, Bracket item){
-	if (isFull(stack)) 
-        return; 
+	if (isFull(stack))
+        return;
     //TODO
+    stack->top++;
+    int current_index = stack->top;
+    stack->array[current_index] = item;
+    return;
 }
 
-// Function to remove an item from stack and return it. 
+// Function to remove an item from stack and return it.
 Bracket pop(Stack* stack){
 	//TODO
 	Bracket top;
+  top = stack->array[stack->top];
+  stack->top--;
 	return top;
-}    
-  
+}
